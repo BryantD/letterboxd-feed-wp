55253a930b097fa217cd9c0b32c9dfe22c6d2b35
BryantD <durrell@innocence.com>
2022-07-02 12:34:50 -0700

    Finished 1.0 of show-tags-cats.py
    Usage:
    
    ./show-tags-cats.py [-h] [-c CONFIG] {categories,tags}
    
    positional arguments:
      {categories,tags}     Specify values to retrieve
    
    optional arguments:
      -h, --help            show this help message and exit
      -c CONFIG, --config CONFIG
    
    Uses the same config file as the main program, but with better error
    handling.

--- show-tags-cats.py
+++ show-tags-cats.py
@@ -16,9 +16,8 @@ def find_wp_api_url(wp_base_url):
         print(f"ERROR: couldn't find WP API link at {wp_base_url}")
         return False
 
-    # Probably a little cleaner to do this with a regex but this works  
-    wp_api_url = response.headers["Link"].partition(">;")[0][1:]
-    
+    wp_api_url = response.links["https://api.w.org/"]["url"]
+
     try:
         response = requests.get(wp_api_url)
     except requests.RequestException as e:
@@ -51,15 +50,23 @@ def main():
         sys.exit()
 
     wp_api_url = find_wp_api_url(config["wp"]["wp_url"])
-    
-    try:
-        response = requests.get(f"{wp_api_url}wp/v2/{args.action}")
-    except requests.RequestException as e:
-        print(f"ERROR: couldn't get {args.action} values: {e}")
-        return False
 
-    for item in response.json():
-        print(f"{item['id']}: {item['name']}")
+    payload = {"page": 1}
+    
+    while payload["page"]:
+        try:
+            response = requests.get(f"{wp_api_url}wp/v2/{args.action}", payload)
+        except requests.RequestException as e:
+            print(f"ERROR: couldn't get {args.action} values: {e}")
+            return False
+
+        for item in response.json():
+            print(f"{item['id']}: {item['name']}")
+            
+        if "next" in response.links:
+            payload["page"] = payload["page"] + 1
+        else:
+            payload["page"] = 0
 
 if __name__ == "__main__":
     main()
